36723c3e9b48e6afec3cfcada565ac30079dc9ce
Sergio Ferreira <88517724+sergioferreiira@users.noreply.github.com>
2023-05-16 21:30:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+// ------------------- lampada -----------------------//
+
+// const lampada = document.querySelector('#objLampada');
+// const acende = document.querySelector('#acenderLampada')
+// const apaga = document.querySelector('#apagarLampada')
+
+// acende.addEventListener('click', () => {
+//     lampada.src = "https://github.com/gabrieldarezzo/helpjs-ravi/blob/master/images/lampada-on.jpg?raw=true"
+// })
+
+// apaga.addEventListener('click', () => {
+//     lampada.src = "https://github.com/gabrieldarezzo/helpjs-ravi/blob/master/images/lampada.jpg?raw=true "
+
+// })
+
+
+// ---------------- CONTADOR DE LETRAS -------------//
+
+
+// const checar = document.querySelector('#limpar');
+
+// checar.addEventListener('click', () => {
+//     const nome = document.querySelector('#texto').value;
+//     const resultado = document.querySelector('#result');
+//     resultado.value = nome.length;
+// })
+
+
+// --------------- CALCULADORA ---------------------//
+
+// const num1 = document.querySelector('#num1');
+// const num2 = document.querySelector('#num2');
+// const resultado = document.querySelector('#result');
+// const clear = document.querySelector('#limpar');
+
+// const mais = document.querySelector('#mais');
+
+// mais.addEventListener('click', somar)
+// menos.addEventListener('click', subtrair)
+// vezes.addEventListener('click', multiplicar)
+// dividir.addEventListener('click', divisao)
+
+// function somar() {
+//     const valor = parseInt(num1.value);
+//     const segundoValor = parseInt(num2.value);
+//     resultado.value = valor + segundoValor;
+// }
+
+// function subtrair() {
+//     const valor = parseInt(num1.value);
+//     const segundoValor = parseInt(num2.value);
+//     resultado.value = valor - segundoValor;
+// }
+
+// function multiplicar() {
+//     const valor = parseInt(num1.value);
+//     const segundoValor = parseInt(num2.value);
+//     resultado.value = valor * segundoValor;
+// }
+
+// function divisao() {
+//     const valor = parseInt(num1.value);
+//     const segundoValor = parseInt(num2.value);
+//     resultado.value = valor / segundoValor;
+// }
+
+// clear.addEventListener('click', limpar)
+
+// function limpar() {
+//     num1.value = "";
+//     num2.value = "";
+//     resultado.value = "";
+
+// }
